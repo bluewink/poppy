dac33a0cf98aea722955e0b9974e212abf84f123
jaeyong <maxwodyd98@gmail.com>
2020-12-21 05:05:44 +0900

    update: mvp 설문진행 넘어가는 화면 생성

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 import Detail from './pages/Detail';
 import Address from './pages/Address';
 import Confirm from './pages/Confirm';
+import Done from './pages/Done';
+
 export default function App() {
   return (
     <MobileSize>
-      <Confirm />
+      <Done />
     </MobileSize>
   );
 }
