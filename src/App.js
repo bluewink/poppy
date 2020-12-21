387e5acfe5e0ee1508c510da6fb956aa6022e53c
bluewink <chandbeen@gmail.com>
2020-12-22 01:19:52 +0900

    update: add route

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Landing from "./pages/Landing.js";
 import Search from "./pages/Search.js";
+import Address from "./pages/Address";
+import Confirm from "./pages/Confirm";
+import Done from "./pages/Done";
+import Detail from "./pages/Detail";
+import {BrowserRouter, Route} from "react-router-dom";
+
 export default function App() {
   return (
-    <MobileSize>
-      <div>Poppy</div>
-      <Header />
-      <Search />
-      {/* <Landing /> */}
-
-    </MobileSize>
+    <BrowserRouter>
+      <MobileSize>
+        <div>Poppy</div>
+        <Header />
+        <Search />
+        <Route path={["/landingpage", "/"]} exact component ={Landing} />
+        <Route path="/takeoffer" exact component={Search} />
+        <Route path="/address" exact component={Address} />
+        <Route path="/confirm" exact component={Confirm} />
+        <Route path="/survey" exact component={Done} />
+        <Route path="/takeofferdetail" exact component={Detail} />
+      </MobileSize>
+    </BrowserRouter>
   );
 }
 
